8f55a74630e31ca3bc34f525ce1e75911cc6e916
IShungite <56173955+IShungite@users.noreply.github.com>
2023-02-17 17:17:07 +0100

    feat: ✨ Add react query

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import { ChakraProvider } from '@chakra-ui/react';
 import { RouterProvider } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import router from './router';
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
     <ChakraProvider>
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
     </ChakraProvider>
   );
 }
