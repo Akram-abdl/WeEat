712b754408ff1bab1a51e5d5f5b87302f9b31077
Akram Abdoul-Wahab <akram.abdl@outlook.fr>
2023-02-17 11:51:34 +0100

    add config firebase

---
+++ src/utils/firebaseSetup.ts
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyAdb8-mB35cahKwXOqBxefzPMWL9P1fHqw',
+  authDomain: 'weeat-e9c21.firebaseapp.com',
+  projectId: 'weeat-e9c21',
+  storageBucket: 'weeat-e9c21.appspot.com',
+  messagingSenderId: '871361007505',
+  appId: '1:871361007505:web:794f4851f2f6e3878fd430',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase Authentication and get a reference to the service
+const auth = getAuth(app);
